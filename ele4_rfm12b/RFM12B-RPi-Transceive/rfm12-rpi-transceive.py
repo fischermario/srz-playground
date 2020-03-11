479869fb943880f35425f50b4182f20dcb2cc3e3
Mario Fischer <mario@localhost.localdomain>
2020-03-11 15:29:34 +0100

    Update example to be Python3 compilant

--- ele4_rfm12b/RFM12B-RPi-Transceive/rfm12-rpi-transceive.py
+++ ele4_rfm12b/RFM12B-RPi-Transceive/rfm12-rpi-transceive.py
@@ -26,7 +26,7 @@ def main():
 	arduino = serial.Serial('/dev/ttyACM0', 57600, timeout=1)
 
 	# First line is empty
-	line = arduino.readline()
+	line = arduino.readline().decode('utf-8')
 
 	# Real
 	line = arduino.readline()
@@ -38,24 +38,24 @@ def main():
 	sendlength = 1
 
 	while True:
-		line = arduino.readline()
+		line = arduino.readline().decode('utf-8')
 		if len(line) > 0:
 			print(line.strip())
 			searchObj = re.search( r'^\[(\d+)\]', line.strip(), re.M|re.I)
 			if searchObj:
 				target = searchObj.group(1)
 				arduino.write(target + "\n")
-				line = arduino.readline()
+				line = arduino.readline().decode('utf-8')
 				if line.strip() == "OK":
-					arduino.write("Jo-Jo-Jo\n")
-					line = arduino.readline()
+					payload = "Jo-Jo-Jo"
+					arduino.write((payload + "\n").encode('utf-8'))
+					line = arduino.readline().decode('utf-8')
 					if line.strip() == "OK":
 						print("SUCCESS!")
 					else:
 						print("FAIL!")
 				else:
 					print("FAIL!")
-				
 
 		time.sleep(1)
 
